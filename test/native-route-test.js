6bceeb2b10a7d125bec7682a914b2fc736c8057d
Derek Kastner <dkastner@gmail.com>
2011-08-29 13:43:04 -0400

    s/train/rail/

--- test/native-route-test.js
+++ test/native-route-test.js
@@ -1,23 +1,26 @@
-var vows = require('vows'),
-  assert = require('assert');
+require('./helper');
+
+var NativeRoute = require('../lib/native-route');
 
 vows.describe('NativeRoute').addBatch({
-  topic: new(NativeRoute('Lansing, MI', 'Chicago, IL')),
+  'instance methods': {
+    topic: new NativeRoute('Lansing, MI', 'Chicago, IL'),
 
-  '#routeAutomobile': function(route) {
-    assert.include(route.automobile(), 'totalDistance');
-    assert.include(route.automobile(), 'segments');
-  },
-  '#routeBus': function(route) {
-    assert.include(route.bus(), 'totalDistance');
-    assert.include(route.bus(), 'segments');
-  },
-  '#routeFlight': function(route) {
-    assert.include(route.flight(), 'totalDistance');
-    assert.include(route.flight(), 'segments');
-  },
-  '#routeRail': function(route) {
-    assert.include(route.rail(), 'totalDistance');
-    assert.include(route.rail(), 'segments');
+    '#automobile': function(route) {
+      assert.include(route.automobile(), 'totalDistance');
+      assert.include(route.automobile(), 'segments');
+    },
+    '#bus': function(route) {
+      assert.include(route.bus(), 'totalDistance');
+      assert.include(route.bus(), 'segments');
+    },
+    '#flight': function(route) {
+      assert.include(route.flight(), 'totalDistance');
+      assert.include(route.flight(), 'segments');
+    },
+    '#rail': function(route) {
+      assert.include(route.rail(), 'totalDistance');
+      assert.include(route.rail(), 'segments');
+    }
   }
 }).export(module);
