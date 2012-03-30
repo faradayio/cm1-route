12df81fd79ff47b2c9a769a80906e7f7753575d8
Derek Kastner <dkastner@gmail.com>
2012-03-30 16:41:31 -0400

    Passing MapquestDirections tests

---
+++ test/directions/mapquest-directions-test.js
@@ -0,0 +1,62 @@
+var _ = require('underscore');
+
+var helper = require('../helper'),
+    assert = helper.assert,
+    sinon = helper.sinon,
+    vows = helper.vows;
+
+var mapquestResult = require('../fixtures/mapquest-result'),
+    directionsBehavior = require('../directions-behavior');
+
+var MapquestDirections = helper.lib.require('./directions/mapquest-directions');
+    MapquestApi = helper.lib.require('./mapquest-api');
+
+var directions = new MapquestDirections();
+directions.originLatLng = new google.maps.LatLng(1, 2);
+directions.destinationLatLng = new google.maps.LatLng(1, 2);
+directions.storeRoute(MapquestDirections.translateRoute(mapquestResult));
+
+vows.describe('MapquestDirections').addBatch({
+  '.translateRoute': {
+    'translates mapquest directions into a GMaps format': function() {
+      var translation = MapquestDirections.translateRoute(mapquestResult);
+      assert.isArray( translation.routes);
+      assert.isObject(translation.routes[0]);
+      assert.isArray( translation.routes[0].legs);
+      assert(         translation.routes[0].legs[0].distance.value > 0);
+      //assert.equal(   translation.routes[0].legs[0].start_address, '1916 Broadway, New York, NY')
+      //assert.equal(   translation.routes[0].legs[0].end_address, '162 Madison Ave, New York, NY')
+      assert.isArray( translation.routes[0].legs[0].steps);
+      assert.isObject(translation.routes[0].legs[0].steps[0]);
+      assert.equal(   translation.routes[0].legs[0].steps[0].travel_mode, 'WALKING');
+      assert.equal(   translation.routes[0].legs[0].steps[0].distance.value, 98.169984);
+      assert.equal(   translation.routes[0].legs[0].steps[0].instructions, 'Start out going north on Broadway toward W 64th St.');
+    }
+  },
+
+  '.generateOverviewPath': {
+    'converts a list of lat/lon numbers to google Lat/Lon objects': function() {
+      var pairs = MapquestDirections.generateOverviewPath(mapquestResult.route.shape.shapePoints);
+      assert.isArray(pairs);
+      assert.equal(pairs.length, mapquestResult.route.shape.shapePoints.length / 2);
+      assert.isObject(pairs[0]);
+      assert.approximately(pairs[0].lat(), 40.772075);
+      assert.approximately(pairs[0].lng(), -73.98207);
+    }
+  },
+
+  '#storeRoute': directionsBehavior.proviesStoreRoute(directions),
+
+  '#fetchMapquest': sinon.testCase({
+    'sends a request to Mapquest API': function() {
+      var fetch = sinon.spy(MapquestApi, 'fetch');
+
+      var cb = sinon.stub();
+      directions.fetchMapquest(cb);
+
+      assert.deepEqual(_.first(fetch.getCall(0).args, 2), ['1,2', '1,2']);
+
+      MapquestApi.fetch.restore();
+    }
+  }),
+}).export(module, { error: false });
---
+++ test/fixtures/mapquest-result.js
@@ -0,0 +1,448 @@
+var mapquestResult = module.exports = {
+  "route": {
+    "hasTollRoad":false,
+    "computedWaypoints":[], "fuelUsed":0,
+    "shape": {
+      "maneuverIndexes":[ 0, 4, 6, 10, 56, 64, 70, 72 ],
+      "shapePoints":[ 40.772075, -73.98207,
+        40.772171, -73.98207,
+        40.772651, -73.982063,
+        40.77296, -73.982063,
+        40.77296, -73.982063,
+        40.773048, -73.982292,
+        40.773048, -73.982292,
+        40.773437, -73.982315,
+        40.773441, -73.982208,
+        40.773441, -73.982208,
+        40.773441, -73.982208,
+        40.769222, -73.982101,
+        40.768795, -73.982086,
+        40.768695, -73.982078,
+        40.768581, -73.982055,
+        40.768455, -73.982009,
+        40.768245, -73.981925,
+        40.768245, -73.981925,
+        40.767894, -73.981781,
+        40.767776, -73.981742,
+        40.767681, -73.981719,
+        40.767562, -73.981697,
+        40.767322, -73.981689,
+        40.767173, -73.981704,
+        40.766807, -73.981758,
+        40.765808, -73.98191,
+        40.76546, -73.981979,
+        40.765125, -73.982063,
+        40.76482, -73.982154,
+        40.764385, -73.982322,
+        40.76403, -73.982467,
+        40.763713, -73.982627,
+        40.763412, -73.982795,
+        40.762981, -73.983039,
+        40.762485, -73.98336,
+        40.761726, -73.983848,
+        40.761726, -73.983848,
+        40.761039, -73.984291,
+        40.760822, -73.984443,
+        40.760593, -73.984573,
+        40.760284, -73.984725,
+        40.760017, -73.98484,
+        40.759712, -73.984962,
+        40.759403, -73.985061,
+        40.758632, -73.985328,
+        40.758373, -73.98542,
+        40.758155, -73.985511,
+        40.758014, -73.985565,
+        40.757881, -73.985641,
+        40.757759, -73.98571,
+        40.757637, -73.985786,
+        40.757343, -73.986007,
+        40.75529, -73.987495,
+        40.75529, -73.987495,
+        40.750373, -73.991058,
+        40.750373, -73.991058,
+        40.750373, -73.991058,
+        40.750373, -73.991058,
+        40.750385, -73.991073,
+        40.75032, -73.991119,
+        40.749698, -73.991561,
+        40.750045, -73.992355,
+        40.749698, -73.991561,
+        40.749698, -73.991561,
+        40.749698, -73.991561,
+        40.749469, -73.991012,
+        40.748508, -73.988708,
+        40.748279, -73.988166,
+        40.74715, -73.985496,
+        40.746479, -73.983901,
+        40.746479, -73.983901,
+        40.746788, -73.983673
+      ],
+      "legIndexes":[ 0 ]
+    },
+    "hasUnpaved":false,
+    "hasHighway":false,
+    "realTime":1623,
+    "boundingBox":{
+      "ul":{ "lng":-73.99156099999999, "lat":40.773441 },
+      "lr":{ "lng":-73.982063, "lat":40.746479 }
+    },
+    "distance":2.437,
+    "time":1623,
+    "informationAreas":[ ],
+    "locationSequence":[ 0, 1 ],
+    "sessionId":"4f74bade-022d-0000-02b7-2c81-d485645bb58e",
+    "locations":[
+      {
+        "latLng":{ "lng":-73.982062, "lat":40.772077 },
+        "adminArea4":"New York County",
+        "adminArea5Type":"City",
+        "adminArea4Type":"County",
+        "adminArea5":"New York",
+        "street":"1916 Broadway",
+        "adminArea1":"US",
+        "adminArea3":"NY",
+        "type":"s",
+        "displayLatLng":{ "lng":-73.982063, "lat":40.772075 },
+        "linkId":21549679,
+        "postalCode":"10023-7001",
+        "sideOfStreet":"R",
+        "dragPoint":false,
+        "adminArea1Type":"Country",
+        "geocodeQuality":"ADDRESS",
+        "geocodeQualityCode":"L1AAA",
+        "adminArea3Type":"State"
+      },
+      {
+        "latLng":{ "lng":-73.98367, "lat":40.74679 },
+        "adminArea4":"New York County",
+        "adminArea5Type":"City",
+        "adminArea4Type":"County",
+        "adminArea5":"New York",
+        "street":"162 Madison Ave",
+        "adminArea1":"US",
+        "adminArea3":"NY",
+        "type":"s",
+        "displayLatLng":{ "lng":-73.983673, "lat":40.746791 },
+        "linkId":774466034,
+        "postalCode":"10016",
+        "sideOfStreet":"L",
+        "dragPoint":false,
+        "adminArea1Type":"Country",
+        "geocodeQuality":"POINT",
+        "geocodeQualityCode":"P1AAA",
+        "adminArea3Type":"State"
+      }
+    ],
+    "hasSeasonalClosure":false,
+    "hasCountryCross":false,
+    "legs":[
+      {
+        "hasTollRoad":false,
+        "index":0,
+        "roadGradeStrategy":[ [ ] ],
+        "hasHighway":false,
+        "hasUnpaved":false,
+        "distance":2.437,
+        "time":1623,
+        "origIndex":-1,
+        "hasSeasonalClosure":false,
+        "origNarrative":"",
+        "hasCountryCross":false,
+        "formattedTime":"00:27:03",
+        "destNarrative":"Proceed to 162 MADISON AVE.",
+        "destIndex":4,
+        "maneuvers":[
+          {
+            "signs":[ ],
+            "index":0,
+            "maneuverNotes":[ ],
+            "direction":1,
+            "narrative":"Start out going north on Broadway toward W 64th St.",
+            "iconUrl":"http://content.mapquest.com/mqsite/turnsigns/icon-dirs-start_sm.gif",
+            "distance":0.061,
+            "time":88,
+            "linkIds":[ ],
+            "streets":[ "Broadway" ],
+            "attributes":0,
+            "transportMode":"WALKING",
+            "formattedTime":"00:01:28",
+            "directionName":"North",
+            "mapUrl":"http://www.mapquestapi.com/staticmap/v3/getmap?type=map&size=225,160&pois=purple-1,40.772075,-73.98207,0,0|purple-2,40.77296,-73.982063,0,0|¢er=40.7725175,-73.9820665&zoom=14&key=Fmjtd|luu7n1u7ng,2g=o5-5rb0h&rand=1906881666&session=4f74bade-022d-0000-02b7-2c81-d485645bb58e",
+            "startPoint":{
+              "lng":-73.98207,
+              "lat":40.772075
+            },
+            "turnType":2
+          },
+          {
+            "signs":[ ],
+            "index":1,
+            "maneuverNotes":[ ],
+            "direction":2,
+            "narrative":"Turn left onto W 65th St/Lincoln Sq.",
+            "iconUrl":"http://content.mapquest.com/mqsite/turnsigns/rs_left_sm.gif",
+            "distance":0.013,
+            "time":19,
+            "linkIds":[ ],
+            "streets":[
+              "W 65th St",
+              "Lincoln Sq"
+            ],
+            "attributes":0,
+            "transportMode":"WALKING",
+            "formattedTime":"00:00:19",
+            "directionName":"Northwest",
+            "mapUrl":"http://www.mapquestapi.com/staticmap/v3/getmap?type=map&size=225,160&pois=purple-2,40.77296,-73.982063,0,0|purple-3,40.773047999999996,-73.982292,0,0|¢er=40.773004,-73.9821775&zoom=15&key=Fmjtd|luu7n1u7ng,2g=o5-5rb0h&rand=1906881666&session=4f74bade-022d-0000-02b7-2c81-d485645bb58e",
+            "startPoint":{
+              "lng":-73.982063,
+              "lat":40.77296
+            },
+            "turnType":6
+          },
+          {
+            "signs":[ ],
+            "index":2,
+            "maneuverNotes":[
+              {
+                "manNote":"Wait approximately 2 min",
+                "manNoteType":"37"
+              }
+            ],
+            "direction":1,
+            "narrative":"Enter 66 St - Lincoln Center.",
+            "distance":0.033,
+            "time":47,
+            "linkIds":[ ],
+            "streets":[ "66 St - Lincoln Center" ],
+            "attributes":0,
+            "transportMode":"WALKING",
+            "formattedTime":"00:00:47",
+            "directionName":"North",
+            "mapUrl":"http://www.mapquestapi.com/staticmap/v3/getmap?type=map&size=225,160&pois=purple-3,40.773047999999996,-73.982292,0,0|purple-4,40.773441,-73.982208,0,0|¢er=40.7732445,-73.98225&zoom=15&key=Fmjtd|luu7n1u7ng,2g=o5-5rb0h&rand=1906881666&session=4f74bade-022d-0000-02b7-2c81-d485645bb58e",
+            "startPoint":{
+              "lng":-73.982292,
+              "lat":40.773048
+            },
+            "turnType":22
+          },
+          {
+            "signs":[ ],
+            "index":3,
+            "maneuverNotes":[
+              {
+                "manNote":"Depart: 3:46 PM From 66 St - Lincoln Center",
+                "manNoteType":"32"
+              },
+              {
+                "manNote":"Travel: 4 Stops",
+                "manNoteType":"34"
+              },
+              {
+                "manNote":"Arrive: 3:52 PM At 34 St - Penn Station",
+                "manNoteType":"33"
+              },
+              {
+                "manNote":"Previous Stop: Times Sq - 42 St",
+                "manNoteType":"35"
+              }
+            ],
+            "direction":6,
+            "narrative":"Take the 1 toward SOUTH.",
+            "distance":1.694,
+            "time":555,
+            "linkIds":[
+
+            ],
+            "streets":[
+              "1"
+            ],
+            "attributes":0,
+            "transportMode":"RAIL",
+            "formattedTime":"00:09:15",
+            "directionName":"Southwest",
+            "mapUrl":"http://www.mapquestapi.com/staticmap/v3/getmap?type=map&size=225,160&pois=purple-4,40.773441,-73.982208,0,0|purple-5,40.750372999999996,-73.991058,0,0|¢er=40.761906999999994,-73.986633&zoom=8&key=Fmjtd|luu7n1u7ng,2g=o5-5rb0h&rand=1906881666&session=4f74bade-022d-0000-02b7-2c81-d485645bb58e",
+            "startPoint":{
+              "lng":-73.982208,
+              "lat":40.773441
+            },
+            "turnType":19
+          },
+          {
+            "signs":[
+
+            ],
+            "index":4,
+            "maneuverNotes":[
+
+            ],
+            "direction":7,
+            "narrative":"Exit 34 St - Penn Station.",
+            "distance":0.151,
+            "time":218,
+            "linkIds":[
+
+            ],
+            "streets":[
+              "34 St - Penn Station"
+            ],
+            "attributes":0,
+            "transportMode":"WALKING",
+            "formattedTime":"00:03:38",
+            "directionName":"West",
+            "mapUrl":"http://www.mapquestapi.com/staticmap/v3/getmap?type=map&size=225,160&pois=purple-5,40.750372999999996,-73.991058,0,0|purple-6,40.749697999999995,-73.99156099999999,0,0|¢er=40.750035499999996,-73.9913095&zoom=14&key=Fmjtd|luu7n1u7ng,2g=o5-5rb0h&rand=1906881666&session=4f74bade-022d-0000-02b7-2c81-d485645bb58e",
+            "startPoint":{
+              "lng":-73.991058,
+              "lat":40.750373
+            },
+            "turnType":23
+          },
+          {
+            "signs":[
+
+            ],
+            "index":5,
+            "maneuverNotes":[
+
+            ],
+            "direction":5,
+            "narrative":"Go southeast on W 32nd St.",
+            "iconUrl":"http://content.mapquest.com/mqsite/turnsigns/rs_straight_sm.gif",
+            "distance":0.46,
+            "time":661,
+            "linkIds":[
+
+            ],
+            "streets":[
+              "W 32nd St"
+            ],
+            "attributes":0,
+            "transportMode":"WALKING",
+            "formattedTime":"00:11:01",
+            "directionName":"Southeast",
+            "mapUrl":"http://www.mapquestapi.com/staticmap/v3/getmap?type=map&size=225,160&pois=purple-6,40.749697999999995,-73.99156099999999,0,0|purple-7,40.746479,-73.983901,0,0|¢er=40.748088499999994,-73.987731&zoom=11&key=Fmjtd|luu7n1u7ng,2g=o5-5rb0h&rand=1906881666&session=4f74bade-022d-0000-02b7-2c81-d485645bb58e",
+            "startPoint":{
+              "lng":-73.991561,
+              "lat":40.749698
+            },
+            "turnType":0
+          },
+          {
+            "signs":[
+
+            ],
+            "index":6,
+            "maneuverNotes":[
+
+            ],
+            "direction":3,
+            "narrative":"Turn left onto Madison Ave.",
+            "iconUrl":"http://content.mapquest.com/mqsite/turnsigns/rs_left_sm.gif",
+            "distance":0.025,
+            "time":35,
+            "linkIds":[
+
+            ],
+            "streets":[
+              "Madison Ave"
+            ],
+            "attributes":0,
+            "transportMode":"WALKING",
+            "formattedTime":"00:00:35",
+            "directionName":"Northeast",
+            "mapUrl":"http://www.mapquestapi.com/staticmap/v3/getmap?type=map&size=225,160&pois=purple-7,40.746479,-73.983901,0,0|purple-8,40.746787999999995,-73.983673,0,0|¢er=40.7466335,-73.983787&zoom=15&key=Fmjtd|luu7n1u7ng,2g=o5-5rb0h&rand=1906881666&session=4f74bade-022d-0000-02b7-2c81-d485645bb58e",
+            "startPoint":{
+              "lng":-73.983901,
+              "lat":40.746479
+            },
+            "turnType":6
+          },
+          {
+            "signs":[
+
+            ],
+            "index":7,
+            "maneuverNotes":[
+
+            ],
+            "direction":0,
+            "narrative":"162 MADISON AVE is on the left.",
+            "iconUrl":"http://content.mapquest.com/mqsite/turnsigns/icon-dirs-end_sm.gif",
+            "distance":0,
+            "time":0,
+            "linkIds":[
+
+            ],
+            "streets":[
+
+            ],
+            "attributes":0,
+            "transportMode":"AUTO",
+            "formattedTime":"00:00:00",
+            "directionName":"",
+            "startPoint":{
+              "lng":-73.983673,
+              "lat":40.746788
+            },
+            "turnType":-1
+          }
+        ],
+        "hasFerry":false
+      }
+    ],
+    "formattedTime":"00:27:03",
+    "options":{
+      "mustAvoidLinkIds":[
+
+      ],
+      "drivingStyle":2,
+      "countryBoundaryDisplay":true,
+      "generalize":0,
+      "narrativeType":"text",
+      "projection":"Equirectangular",
+      "locale":"en_US",
+      "avoidTimedConditions":true,
+      "destinationManeuverDisplay":true,
+      "enhancedNarrative":true,
+      "filterZoneFactor":-1,
+      "timeType":1,
+      "maxWalkingDistance":-1,
+      "routeType":"MULTIMODAL",
+      "transferPenalty":-1,
+      "stateBoundaryDisplay":true,
+      "walkingSpeed":-1,
+      "maxLinkId":0,
+      "arteryWeights":[
+
+      ],
+      "tryAvoidLinkIds":[
+
+      ],
+      "unit":"M",
+      "routeNumber":0,
+      "shapeFormat":"raw",
+      "maneuverPenalty":-1,
+      "useTraffic":false,
+      "returnLinkDirections":false,
+      "avoidTripIds":[
+
+      ],
+      "manmaps":"true",
+      "highwayEfficiency":22,
+      "sideOfStreetDisplay":true,
+      "cyclingRoadFactor":1,
+      "urbanAvoidFactor":-1
+    },
+    "hasFerry":false
+  },
+  "info":{
+    "copyright":{
+      "text":"© 2012 MapQuest, Inc.",
+      "imageUrl":"http://api.mqcdn.com/res/mqlogo.gif",
+      "imageAltText":"© 2012 MapQuest, Inc."
+    },
+    "statuscode":0,
+    "messages":[
+
+    ]
+  }
+};
