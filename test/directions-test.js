a279938d2e7d12d5abd5d41aa3b5521c87be9253
Derek Kastner <dkastner@gmail.com>
2012-05-21 22:38:13 -0400

    Combine DrivingSegments for IE's sake

--- test/directions-test.js
+++ test/directions-test.js
@@ -7,12 +7,10 @@ var Cm1Result = require('./fixtures/cm1-result'),
     GoogleResult = require('./fixtures/google-result');
 
 var Directions = helper.lib.require('./directions'),
-    SubwayingSegment = helper.lib.require('./segment/subwaying-segment');
+    DrivingSegment = helper.lib.require('./segment/driving-segment'),
+    LightRailingSegment = helper.lib.require('./segment/light-railing-segment'),
+    SubwayingSegment = helper.lib.require('./segment/subwaying-segment'),
     WalkingSegment = helper.lib.require('./segment/walking-segment');
-
-var directions = new Directions('Lansing, MI', 'Ann Arbor, MI', 'DRIVING');
-directions.calculateDistance = sinon.stub();
-directions.storeRoute(GoogleResult.driving);
 
 vows.describe('Directions').addBatch({
   '#getEmissions': {
@@ -20,30 +18,24 @@ vows.describe('Directions').addBatch({
       var directions = new Directions('Lansing, MI', 'Ann Arbor, MI', 'DRIVING');
       directions.calculateDistance = sinon.stub();
       directions.storeRoute(GoogleResult.driving);
-      directions.eachSegment(function(segment) {
-        sinon.spy(segment, 'getImpacts');
-      });
       directions.segmentEmissionsCallback = sinon.spy();
 
       directions.getEmissions(this.callback, directions.segmentEmissionsCallback);
     },
     
-    'gets emissions for all segments': function(err, directions) {
-      directions.eachSegment(function(segment) {
-        sinon.assert.called(segment.getImpacts);
-      });
-    },
     'calls back with directions when all segments have calculated emissions': function(err, directions) {
       assert.instanceOf(directions, Directions);
     },
-    'calls back for each segment': function(err, directions) {
-      assert.equal(directions.segmentEmissionsCallback.callCount, directions.segments.length);
+    'calls back for each type of segment': function(err, directions) {
+      assert.equal(directions.segmentEmissionsCallback.callCount, 1);
     }
   },
 
   '.events': {
     '.onSegmentGetEmissionEstimate': {
       'updates the total emissions': function() {
+        var directions = new Directions('Lansing, MI', 'Ann Arbor, MI', 'DRIVING');
+        directions.storeRoute(GoogleResult.driving);
         var evt = Directions.events.onSegmentGetEmissionEstimate(directions, sinon.stub(), sinon.stub());
         directions.totalEmissions = 0;
         evt(null, { carbon: 14254.4678 });
@@ -53,17 +45,28 @@ vows.describe('Directions').addBatch({
   },
 
   '#totalTime': {
-    "sums each segment's duration and pretty prints the result": function() {
+    topic: function() {
+      var directions = new Directions('Lansing, MI', 'Ann Arbor, MI', 'DRIVING');
+      directions.calculateDistance = sinon.stub();
+      directions.storeRoute(GoogleResult.driving);
+      return directions;
+    },
+
+    "sums each segment's duration and pretty prints the result": function(directions) {
       assert.equal(directions.totalTime(), '6 mins');
     },
-    'returns empty string if there are no segments': function() {
+    'returns empty string if there are no segments': function(directions) {
       directions.segments = [];
       assert.equal(directions.totalTime(), '');
     }
   },
 
   '#isAllWalkingSegments': {
-    'returns true if all segments are walking segments': function() {
+    topic: function() {
+      return new Directions('Lansing, MI', 'Ann Arbor, MI', 'DRIVING');
+    },
+
+    'returns true if all segments are walking segments': function(directions) {
       directions.segments = [
         new WalkingSegment(0, {}),
         new WalkingSegment(0, {}),
@@ -73,7 +76,7 @@ vows.describe('Directions').addBatch({
 
       assert.isTrue(directions.isAllWalkingSegments());
     },
-    'returns false if at least one segment is not a walking segment': function() {
+    'returns false if at least one segment is not a walking segment': function(directions) {
       directions.segments = [
         new WalkingSegment(0, {}),
         new WalkingSegment(0, {}),
@@ -84,4 +87,42 @@ vows.describe('Directions').addBatch({
       assert.isFalse(directions.isAllWalkingSegments());
     }
   },
+
+  '#combineSegments': {
+    topic: function() {
+      return new Directions('Lansing, MI', 'Ann Arbor, MI', 'DRIVING');
+    },
+
+    'combines like driving segments': function(directions) {
+      directions.segments = [
+        new WalkingSegment(1, {distance: { value: 2}, duration:   { value: 3 },
+          travel_mode: 'WALKING' }),
+        new WalkingSegment(2, {distance: { value: 3}, duration:   { value: 4 },
+          travel_mode: 'WALKING' }),
+        new SubwayingSegment(3, {distance: { value: 5}, duration: { value: 6 },
+          travel_mode: 'SUBWAYING' }),
+        new DrivingSegment(4, {distance: { value: 7},
+          travel_mode: 'DRIVING' }),
+        new DrivingSegment(5, {distance: { value: 8},
+          travel_mode: 'DRIVING' }),
+        new LightRailingSegment(6, {distance: { value: 7}, duration: { value: 8 },
+          travel_mode: 'LIGHTRAILING' }),
+        new WalkingSegment(7, {distance: { value: 9}, duration:   { value: 10 },
+          travel_mode: 'WALKING' })
+      ];
+
+      var segments = directions.combineSegments();
+
+      assert.equal(segments.length, 6);
+
+      assert.instanceOf(segments[0], WalkingSegment);
+      assert.instanceOf(segments[1], WalkingSegment);
+      assert.instanceOf(segments[2], SubwayingSegment);
+      assert.instanceOf(segments[3], LightRailingSegment);
+      assert.instanceOf(segments[4], WalkingSegment);
+
+      assert.instanceOf(segments[5], DrivingSegment);
+      assert.equal(segments[5].distance, 0.015);
+    }
+  }
 }).export(module, { error: false });
